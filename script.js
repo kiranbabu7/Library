d5f5faefbb271d74c57ed202ef7513e2aeda5f1c
kiranbabu <kiranbabu7675@gmail.com>
2021-11-27 22:53:40 -0800

    Command line version of Library app

---
+++ script.js
@@ -0,0 +1,26 @@
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+  //   status(){
+  //       this.read? 'Completed Reading': 'Not read yet'
+  //   }
+  info() {
+    console.log(
+      `${this.title} by ${this.author}, ${this.pages} pages,${
+        this.read ? "Completed Reading" : "Not read yet"
+      } `
+    );
+  }
+}
+
+const thinkAndGrowRich = new Book(
+  "Think And Grow Rich",
+  "Nepoleon Hill",
+  320,
+  true
+);
+const atomicHabits = new Book("Atomic Habits", "Nepoleon Hill", 400, false);
